ad95ef8dd3af5e6e6835d65ca30e383d98281aaa
namseungsik <tmd7031@naver.com>
2023-05-23 21:13:46 +0900

    섹션5완료

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    hello
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue'
 
 export default {
-  name: 'App',
-  methods: {
-    fetchData() {
-      console.log('hi');
-    }
+  components: {
+    ToolBar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    padding: 0;
+    margin: 0;
+  }
 </style>
