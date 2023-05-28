e00bc5fdcb03e404493648f77d3863f1f9edd925
namseungsik <tmd7031@naver.com>
2023-05-28 20:57:49 +0900

    스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -22,6 +22,22 @@ export default {
     padding: 0;
     margin: 0;
   }
+  /* a태그 아래 밑줄 제거 */
+  a {
+    color: #34495e;
+    text-decoration: none;
+  }
+
+  /* 툴바쪽 선택된 항목 밑줄 */
+  a.router-link-exact-active {
+    text-decoration: underline;
+  }
+
+  a:hover {
+    color: #42b883;
+    text-decoration: underline;
+  }
+
   .page-enter-active,
   .page-leave-active {
     transition: opacity 0.5s ease;
--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk" v-bind:key="item.id">
-      <router-link :to="`item/${item.id}`">{{item.title}}</router-link>
-      <small>{{item.time_ago}} by {{item.user}}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedAsk" :key="item.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{item.points}}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link :to="`item/${item.id}`">{{item.title}}</router-link>
+          </p>
+          <small class="link-text">
+            {{item.time_ago}} by {{item.user}}
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -37,6 +50,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .news-list {
+    margin: 0;
+    padding: 0;
+  }
+  .post {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+  }
+  .points {
+    width: 80px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+  }
+  .news-title {
+    margin: 0;
+  }
+  .link-text {
+    color: #828282;
+  }
 </style>
